40c1aa0db8d0eef7fa1e07ab271c4bf6bbb9670e
Victor Gazolli <victorlpgazolli@gmail.com>
2020-04-09 22:25:32 -0300

    [add] tp6 asm file

---
+++ Aula 7/VictorGazolliTP6/main.asm
@@ -0,0 +1,21 @@
+; TP-06:
+;Usando o template em assembly, declare a todas as portas como saida, e fa�a cada uma delas alternarem de estado dentro de um loop infinito;
+
+
+.INCLUDE "atmega328p.inc"
+.device		atmega328P
+.nolist
+.list
+.ORG 0x0000
+	rjmp start
+	
+start:
+	ldi r16, 0xFF
+	out ddrb, r16 ; all ports b output
+	ldi r17, 0xFF ; ALL HIGH
+	ldi r18, 0x00 ; ALL LOW
+	
+loop:
+	out portb, r17		; portb -> high
+	out portb, r18		; portb -> low
+rjmp LOOP                 
